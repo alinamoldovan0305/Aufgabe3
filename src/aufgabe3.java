526ae63a4ca11e500a3c256a3e0929254f50a2bf
Moldovan Alina <amoldovan157@gmail.com>
2025-10-20 14:27:53 +0300

    2-ter Unterpunkt aufg3

--- src/aufgabe3.java
+++ src/aufgabe3.java
@@ -13,5 +13,42 @@ public class aufgabe3 {
         ergebnis[0]=übertrag;
         return ergebnis;
     }
+    public static int VergleichenvonArrays(int[] zahl1, int[] zahl2) {
+        for (int i = 0; i < zahl1.length; i++) {
+            if (zahl1[i] > zahl2[i]) return 1;
+            if (zahl1[i] < zahl2[i]) return -1;
+        }
+        return 0;
+    }
 
+    public static int[] diff2Arrays(int zahl1[], int zahl2[]) {
+        int cmp = VergleichenvonArrays(zahl1,zahl2);
+        int[] ergebnis = new int[zahl1.length + 1];
+        int ausleihen = 0;
+        int[] unten,oben;
+        if (cmp == 0) {
+            return new int[zahl1.length]; // toate 0
+        }
+        if(cmp == 1) {
+            oben=zahl1;
+            unten=zahl2;
+        } else {
+            oben=zahl2;
+            unten=zahl1;
+        }
+        for (int i=oben.length-1; i>=0; i--) {
+            int diff=oben[i]-ausleihen;
+            int untenZahl=unten[i];
+
+            if (diff<untenZahl) {
+                diff+=10;
+                ausleihen=1;
+            } else {
+                ausleihen=0;
+            }
+
+            ergebnis[i]=diff-untenZahl;
+        }
+        return ergebnis;
+    }
 }
